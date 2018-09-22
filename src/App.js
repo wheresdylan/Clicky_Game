56808dce380dec763a3d0f517c6e707038df2233
Dylan Marini <dylan.marini@g.austincc.edu>
2018-09-22 14:38:44 -0500

    added static nav bar

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
+import Nav from "./components/Nav";
 import "./App.css";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
+    message: "Click an image to begin!",
+    topScore: 0,
+    curScore: 0,
     friends
-  };
-
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Nav 
+           message={this.state.message}
+           curScore={this.state.curScore}
+           topScore={this.state.topScore}
+        />
+        <Title></Title>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
