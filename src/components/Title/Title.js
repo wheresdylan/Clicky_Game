1cabd5e547d738d4f6fc17d0f8f00f17a11b3f41
Dylan Marini <dylan.marini@g.austincc.edu>
2018-09-22 13:55:38 -0500

    finished with static cards and title

---
+++ src/components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="title">
+    <h1>Zombie Clicky Game</h1>
+    <p>Click on a zombie to earn points, but do not click on the same zombie twice!</p>
+    </div>
+);
+export default Title;
